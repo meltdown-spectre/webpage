840c7d2d7b5ca0d4131a0221a8519fd0203b4db1
Stephan Schiller <StephanSchiller@Stephans-MacBook-Pro-4.local>
2019-11-05 15:54:25 +0800

    added Meltdown and Spectre part

--- my-app/src/Meltdown.js
+++ my-app/src/Meltdown.js
@@ -10,7 +10,140 @@ class Meltdown extends React.Component{
           </div>
         <div class = "Letters">
             <header className="Headers">Meltdown</header>
-            <p>These are the details of Meltdown</p>
+            <p>Discovered and reported independently by 3 teams in 2018, Meltdown is a 
+              hardware issue found in some processors (Intels, ARMs and IBM POWERs) which 
+              overcomes memory isolation, by exploiting a race condition between memory 
+              read and privilege check. This opens up the possibility for a program to read 
+              memory you have no privilege to access. For example, a user process that is vulnerable 
+              to Meltdown will be able to access the kernel memory, which should be impossible under 
+              normal circumstances.
+              Meltdown is not an exploitation of software vulnerability. Instead, it is an exploitation 
+              of side-channel information that is available on most modern processors. An attacker 
+              with access to the vulnerable processor will be able to exploit the information in it by 
+              running a malicious program to retrieve a dump of the entire kernel address space and mapped 
+              physical memory. <br></br>
+              So why is Meltdown dangerous? The kernel stores encryption keys, passwords or even physical 
+              pages of other processes. These sensitive data will be compromised due to Meltdown and other 
+              users could potentially use these sensitive information to cause harm to others or to the user 
+              himself.<br></br>
+              <div class="SubHeader">
+                Description
+              </div>
+              The full Meltdown attack consists of 2 building blocks:
+              Speculatively executing instructions that should never occur in the executed path.
+              Monitoring the effect of the speculatively executed instructions on the architectural state to 
+              leak the secret. In the first block, these instructions are known as transient instructions and 
+              they occur almost all the time. To maximise performance, the CPU will constantly execute instructions
+               ahead of the current instruction. As such, this brings about a side channel vulnerability if the instructions 
+               executed relies on a secret value. [1] As mentioned previously, kernel memory is not accessible by user processes 
+               and an unprivileged access to it by the process will cause an exception to be thrown. This exception thrown 
+               could be dealt with using exception handling or suppression. A malicious attacker can handle the exception by 
+               installing a signal handler to catch a specific exception and runs his code to prevent the process from terminating. 
+              In the next segment of the attack, due to the (speculative) execution of the instruction that should not have occurred, 
+              there might be a change in the architectural state such as the state of the cache. This change in state will then be passed 
+              through a covert channel and the attacker will be able to deduce what the leaked secret is.
+              In the next part of the section, we demonstrate a walkthrough of the Meltdown attack.
+              <div class="SubHeader">
+              Walkthrough/Demo
+              </div>
+              In this walkthrough, we split the attack into 2 major parts. The first step of the attack uses speculative execution to 
+              cause microarchitectural changes such as affecting the state of the cache and the second step would be to observe these changes 
+              using a technique called flush+reload. To understand how we are able to retrieve the kernel memory, we will first have to 
+              understand the flush+reload technique.
+              <div class="SubHeader">
+               Flush + Reload technique
+              </div>
+              The flush+reload technique allows users to detect if a memory location has been accessed within a period of time. 
+              The general idea behind this technique is to observe the differences in the time taken for retrieval of data when a 
+              memory location is accessed the first time and its subsequent accesses. Using this technique, we can observe that 
+              memory access time is slow initially but becomes much faster in the subsequent access of the same memory location. 
+              This difference in timings between the first access and subsequent accesses is due to the caching of the data. 
+              As the first memory access resulted in a cache miss, the process would have to read the data from the physical 
+              memory which causes the process to incur additional overhead cost. After retrieving the data from the physical memory, 
+              the data is then stored in the cache for subsequent access. As a result, subsequent access to that same memory 
+              location resulted in a cache hit and the time to retrieve the data from the memory is greatly reduced.
+              Using the reasoning above, we will be able to detect if a process uses a memory address. Below describes the detection process:<br></br>
+              1. Suppose we would like to detect if process C accesses/uses the memory location A. <br></br>
+              2. First, we will initialise the cache by flushing A from the cache. We are able to flush the cache using the clflush instruction.
+              <div class = "code">
+                _mm_clflush(Address[A])
+              </div>
+              3. Next, we will run the process C. <br></br>
+              4. After C completes its execution, we will access A separately and measure how long it takes to retrieve its value. <br></br>
+              5. This can be done by counting the number of clock cycles it takes to access the memory. We are able to measure the access 
+              time accurately by finding the difference in time using the rdtsc (read time-stamp counter) instruction. <br></br>
+              <div class="code">
+                time1 = __rdtscp(&A) <br></br>
+                A    = *addr; // addr: Address of A <br></br>
+                time2 = __rdtscp(&A) - time1;
+              </div>
+              6. A short access time would mean that C had used A during its execution. <br></br>
+              7. On the other hand, a long access time would mean that A is not in the cache. 
+              However, we cannot be certain that C did not use A during its execution as it 
+              is also possible that the data were accessed in a way that caused A to be replaced in the cache. <br></br>
+              8. To determine what a 'short access' and 'long access' means, we will run a few tests using 
+              rdtsc instruction to find out what is the average number of cycles to access cached memory and 
+              the number of cycles to access uncached memory. <br></br>
+              9. After retrieving the average number of cycles, we will set a 'threshold' value for cache access 
+              cycles, which separates a 'short' and 'long' access. 
+              <div class="SubHeader">
+                Speculative execution
+              </div>
+              Moving on, we will observe how speculative execution can cause microarchitectural changes. 
+              As mentioned in Section 2, speculative execution is an optimization that modern CPUs use 
+              to maximize the usage of the CPU.<br></br>
+              Consider the following piece of code that is executed by a user-level process: <br></br>
+              <div class="code">
+                void meltdown_asm(unsigned long kernel_data_addr)&#123; <br></br>
+                char kernel_data = 0;<br></br>
+                kernel_data = *(char*)kernel_data_addr; // Line 1. Illegal access, no permission <br></br>
+                array[kernel_data*4096+DELTA] += 1; // Line 2. Speculatively executed <br></br>
+                &#125;
+              </div>
+              Code 2: Meltdown code <br></br>
+              <br></br>
+              When the CPU executes line 1, it fetches the data as well as check the access rights of the process 
+              in parallel. If the data returns before the access rights check is complete, the CPU would have all 
+              it needs to execute line 2. Therefore, instead of idling, the processor will speculatively execute 
+              line 2. As such, line 2 will access a memory location based on the data that was read in line 1.<br></br>
+              Once the access right check returns, the CPU realizes that this is an illegal memory access and discards 
+              the effects of line 1. This is done by setting a status bit in the reorder buffer to indicate a fault, and 
+              when the load data operation commits, the pipeline is flushed and an exception is thrown.<br></br>
+              However, although the pipeline is flushed, the cache had already been affected. The kernel_data is of 
+              type char and has 256 values. Hence, one of the 256 possible cache lines would have already been accessed. 
+              This effect can be observed by the flush+reload technique and hence, we are able to determine the value 
+              of kernel_data by determining which cache line has been accessed. <br></br>
+              Code 2 shows how we are able to determine the value of kernel_data. We first check each of the 256 cache 
+              lines and we are able to deduce that the cache line that has a short access time was accessed. 
+              The accessed cache line was chosen based on the value of kernel_data. Hence, we will be able to determine 
+              what that value was.
+
+              <div class="code">
+                void reloadSideChannel()<br></br>
+                &#123; <br></br>
+                int junk=0; <br></br>
+                register uint64_t time1, time2; <br></br>
+                volatile uint8_t *addr; <br></br>
+                int i; <br></br>
+                for&#40;i=0; i&#60;256;i++&#41;&#123; // check which of the 256 cache lines were accessed <br></br>
+                    addr = &array[i*4096 + DELTA]; <br></br>
+                    time1 = __rdtscp(&junk); // calculating the differences in clock cycles <br></br>
+                    junk = *addr; <br></br>
+                    time2 = __rdtscp(&junk) - time1; <br></br>
+                    if &#40;time2 &#60;&#61; CACHE_HIT_THRESHOLD&#41;&#123;  <br></br>
+                printf&#40;"array[%d*4096 + %d] is in cache.\n", i, DELTA&#41;;<br></br>
+             &#125;
+              </div>
+              Code 3
+              <br></br>
+              <br></br>
+              We combine these two parts to get the meltdown attack. In order to demonstrate this attack, 
+              we planted a secret value inside the kernel using procfs. We will then read the planted secret 
+              from a user-level process using the same techniques above. <br></br>
+              <br></br>
+              The full demonstration code can be seen in Appendix A. <br></br>
+              We included several optimizations in the code (Refer to Appendix A) to increase the chances of a correct read.
+              </p>
         </div>
         </div>
       )
--- my-app/src/Spectre.js
+++ my-app/src/Spectre.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Sidebar from './Sidebar';
-
+import SpectreCode from './resources/spectreCode.png';
 class Spectre extends React.Component {
   render() {
     return (
@@ -10,7 +10,151 @@ class Spectre extends React.Component {
         </div>
         <div>
           <header className="Headers">Spectre</header>
-          <p>These are the details of Spectre</p>
+          <p>Meltdown exploits privilege escalation, however, Spectre does not. 
+            Instead, Spectre requires tailoring to the user process’s software environment. 
+            This attack generally affects CPUs that support speculative execution. 
+            To recall, Spectre attacks mislead processors to undergo speculative execution 
+            that should not have been executed under normal circumstances. (Section 1) 
+            This section will provide a more in-depth look into the Spectre attacks.
+          <div class="SubHeader">
+           Description
+          </div>
+          Unauthorised copying, transferring and retrieving of data from a computer or a 
+          server is known as data exfiltration. Spectre attacks combines both speculative 
+          execution and data exfiltration to violate memory isolation. In general, the 
+          attack starts with the insertion of a malicious instructions within the process address 
+          space. These instructions, when executed, leaks the contents of the user’s memory or 
+          registers through a covert channel. The subsequent step would involve the attacker tricking 
+          the CPU into speculatively execute the malicious instructions. Once the sequence of instructions 
+          is executed, the attacker will deduce the sensitive data that was received in the covert channel.<br></br>
+          
+          Although this speculative execution will be reverted back eventually, the effect of this execution 
+          is similar to that of Meltdown. There will be microarchitectural changes made to components such 
+          as the cache and the leaked data can then be retrieved using the Flush + Reload method mentioned in the previous section. <br></br>
+
+          The above describes how Spectre attacks work in general. Now, we will now delve deeper into the more 
+          common variants of Spectre: Variant 1, Variant 2 and Variant 4. <br></br>
+
+          <div class="SubHeader">
+            Spectre Variant 1 - Bounds Check Bypass
+          </div>
+          This vulnerability allows attackers to use the speculative execution of conditional branch instructions to 
+          leak information that are usually not accessible to them. Attackers take advantage of the “confused deputy” 
+          code that is used for bounds checking, like checking if the index is inbound for an array. Speculative 
+          execution of the memory access to out of bounds memory can leak information to the attackers. <br></br>
+          <br></br>
+          This variant involves the attacker mistraining the branch predictor into mispredicting the branch.
+           <div class="code">
+            if (input &#60; array_size) &#123;   #Checks if input in not out of bounds <br></br>
+              val = data[array[input]];  #Read data<br></br>
+            &#125;
+          </div>
+          Using the example given above, the attack can be split into 2 phases: Mistraining phase and 
+          Exploiting phase. In the mistraining phase, the attacker will constantly pass a value within 
+          the bounds as the input. This will train the branch predictor to expect the “if” result to be true. 
+          Subsequently, in the exploit phase, the attacker will input a value that is out of bounds. 
+          Since the predictor expects the condition to be true, the process will execute the instruction 
+          within the clause and retrieve the “val” from an array which has a value out of its bounds. The 
+          value read will be stored and remain in the cache even after the condition results in false and the 
+          execution is reverted. <br></br>
+          <br></br>
+          The attacker will then be able to determine the leaked memory using the flush + reload technique.
+          <div class="SubHeader">
+              Spectre Variant 2 - Branch Target Injection
+          </div>
+          This vulnerability exploits the indirect branch predictors to direct instructions that are 
+          speculatively executed after an indirect branch instruction to the gadget. A gadget is a sequence 
+          of carefully chosen instructions that are in the processors’ memory. Attackers can train the indirect 
+          branch predictors to speculatively execute malicious code to leak information to them. 
+          Instead of training the branch prediction like in variant 1, the attacker trains the Branch Target 
+          Buffer (BTB) into mispredicting a branch from an indirect branch instruction to the address of the 
+          gadget. This will result in the gadget being speculatively executed and the consequences would be 
+          similar to that of variant 1. <br></br>
+          To carry out this variant of Spectre attack, the attacker finds the gadget’s virtual address in the 
+          user virtual memory and trains the BTB to indirect branch to that address. To train the BTB, the 
+          attacker uses his own virtual address space. What actually resides in the attacker’s address space 
+          does not matter as long as the virtual address is similar to that of the user’s address. Once the BTB 
+          is mistrained to direct indirect branch to that virtual address, the attacker will be able to get a 
+          hold of the user’s leaked data.
+          <div class="SubHeader">
+              Spectre Variant 4 - Speculative Store Bypass
+          </div>
+          This vulnerability uses the memory dependence prediction feature of modern processors which can 
+          predict the memory address of load and store instructions before their addresses are known. The 
+          memory dependence prediction feature allows load and store instructions to speculatively execute 
+          before their addresses are known. Attackers can create “confused deputy” code to speculatively execute 
+          load instructions with memory addresses to sensitive data and leak information to them.<br></br>
+          This exploit involves 3 main steps: <br></br>
+          1. Slowly storing the data at the memory location. <br></br>
+          2. Quickly load that data from that location. <br></br>
+          3. Make changes to the cache. <br></br>
+          <br></br>
+          For this attack to work, the attacker would have to make the storing of data depend on the previous 
+          instruction. As a result, the processor would have to wait before the data could be stored and the 
+          loading of the data will be speculatively executed. The data that is loaded will be stored in the 
+          cache. Once the processor realise that it is storing and loading from the same address, the previous 
+          value loaded will be reverted and the new value is loaded. However, there were already changes made to 
+          the cache and the leaked data could be retrieved in a similar way as mentioned above. 
+          <div class="SubHeader">
+              Walkthrough/Demo
+          </div>
+          As this attack also relies on the changes in the cache, we will make use of the flush + reload 
+          technique previously used in the Meltdown Walkthrough. In addition, we will also use the same method 
+          of checking the values in the cache (Code 3). In this section, we will be demonstrating a Spectre 
+          variant 1 attack. <br></br>
+          Suppose the victim has the following code in their program. <br></br>
+          <div class="code">
+              if (x &#60; array_size) &#123; <br></br>
+	            return array[x];<br></br>
+              &#125;else&#123;<br></br>
+	              return 0;<br></br>
+                &#125;
+          </div>
+          Code XX: The victim’s code vulnerable to the Spectre attack.<br></br>
+          <br></br>
+          We will first train the branch predictor to predict that the if-condition will always 
+          return true. This can be done by consistently looping a value that is within the size of 
+          the array as demonstrated below.<br></br>
+            <div class="code"> 
+            for (i = 0; i &#60; 20; i++) &#123; <br></br>
+              _mm_clflush(& array_size); #flush the values stored in the cache <br></br>
+              victim_code(i); # contains the victim’s code above. Assuming array_size > 20. <br></br>
+              &#125;
+            </div>
+          Code XX: Part of the attacker’s program that trains the branch predictor. <br></br>
+          <br></br>
+          Right after the execution of the code above, we can now input any value larger than the array size. 
+          For understanding purposes, we will demonstrate this by using the address of the secret itself. 
+          However, in actual scenarios, the address of the sensitive data are not known and this retrieval 
+          will be done using any values that is larger than the size of the array. <br></br>
+          As the value retrieve is a single variable, we are able to continuously extracted data stored in a 
+          contiguous segment of memory by incrementing the value of x. This is reflected in Code XX below. <br></br>
+          <div class = "code">
+          for(int j=0; j&#60;len;j++)&#123; <br></br>
+	              for (i=0; i&#60;256; i++) <br></br>
+                     hits[i] = 0; #number of times the character is found in the cache <br></br>
+	          for (i = 0; i&#60;1000; i++) &#123; <br></br>
+                 spectre_attack(larger_x); # trains the branch predictor and perform attack <br></br>         
+                 reloadSideChannelImproved(); # same method as Meltdown <br></br>
+              &#125; <br></br>
+              int max = 0; <br></br>
+              for (i = 0; i &#60;256; i++)&#123; <br></br>
+		             if(hits[max] &#60;hits[i]) max = i; #find the value with highest number of cache hits <br></br>
+              &#125;
+              printf("Reading secret value at %p = ", (void*)larger_x); <br></br>
+              printf("The secret value is %c\n", max); <br></br>
+              larger_x++; # increment the out of bound value to retrieve the next character <br></br>
+            &#125;
+          </div>
+          Code XX: Method used to retrieve the values stored in the memory.<br></br>
+          <br></br>
+          The full demonstration code for the Spectre variant 1 attack can be found in Appendix B. <br></br>
+          For our demonstration, the “secret” that we want to retrieve from the victim’s memory is “My password is cs3235project”. 
+          By running the file in Appendix B, we are able to extract the “secret” as seen in Figure XX. <br></br>
+          <br></br>
+          <img class="Picture4" src={SpectreCode} alt="SpectreCode"></img>
+          <br></br>
+        </p>
         </div>
       </div>
     )
